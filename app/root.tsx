c486f6467727287fcb1151dd82294b6d7c80038c
Joe Davis <jdavis@grayshift.com>
2022-09-19 11:41:22 -0500

    trying to add tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,22 @@
-import type { MetaFunction } from '@remix-run/node'
 import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react'
+import styles from './tailwind.css'
+
+import type { LinksFunction, MetaFunction } from '@remix-run/node'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
   title: 'New Remix App',
   viewport: 'width=device-width,initial-scale=1',
 })
+
+export const links: LinksFunction = () => [
+  { rel: 'stylesheet', href: styles },
+  {
+    rel: 'icon',
+    href: '/favicon.ico',
+    type: 'image/ico',
+  },
+]
 
 export default function App() {
   return (
